1e29a2a85eba834c3c03288fc68a9dc3bd1278ca
Markko Paas <markko.paas@gmail.com>
2019-12-09 18:28:09 +0100

    signup and login

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const MemoryStorage = require('memorystorage');
+const userStorage = new MemoryStorage('users');
 
 const app = express();
 const port = 3001;
 
+const saltRounds = 10;
+
 function getAuthUser(req) {
+    // get user by cookie
     // return { name: 'johnny' };
+}
+
+function checkPassword(password, user) {
+    console.log('calling compare', password, user.hash);
+    return bcrypt.compare(password, user.hash);
+}
+
+function createUser(data, cb) {
+    const user = { username: data.username };
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(data.password, salt, function (err, hash) {
+            user.hash = hash;
+            console.log('storing user ', user);
+            userStorage.setItem(user.username, user);
+            cb();
+        });
+    });
 }
 
 app.use(express.static('static'));
@@ -15,8 +38,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/login', (req, res) => {
     console.log('here');
     console.log(JSON.stringify(req.body, null, 2));
-    //todo set cookie
-    res.redirect('/');
+    if (!(req.body && req.body.username && req.body.password)) {
+        res.redirect('/login.html?no_credentials');
+        return;
+    }
+    const user = userStorage.getItem(req.body.username);
+    console.log('found user', user);
+    if (!user) {
+        res.redirect('/login.html?user_not_found');
+        return;
+    }
+
+    checkPassword(req.body.password, user)
+        .then((authenticated) => {
+        if (!authenticated) {
+            res.redirect('/login.html?invalid_password');
+            return;
+        }
+        console.log('login success', user);
+        //todo set cookie
+        res.redirect('/?success');
+    })
+        .catch(console.error);
+});
+
+
+app.post('/signup', (req, res) => {
+    if (!(req.body && req.body.username && req.body.password)) {
+        console.log('invalid signup');
+        res.redirect('/signup.html');
+    }
+    createUser(req.body, () => {
+        console.log('signup done');
+        res.redirect('/');
+    });
 });
 
 app.get('/', (req, res) => {
